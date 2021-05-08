84271809f1f21b1757910c09ae0fabba823c5945
Vince Developer023 <82177794+ngodev023@users.noreply.github.com>
2021-05-07 17:45:51 -0700

    project 6 test upload

---
+++ Project 6 Node and Express Site/app.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const data = require('./data.json');
+const routes = require('./routes/index.js')
+
+const app = express();
+app.set('view engine', 'pug')
+app.use('/static', express.static('public'))
+
+app.use(routes)
+
+// handles unfound urls: 404 error status setter
+app.use((req, res, next)=>{
+    const err = new Error('The page you are looking for does not exist!');
+    err.status = 404;
+    console.log(err.message)
+    next(err);
+})
+
+// Global Error handler
+app.use((err, req, res, next)=>{
+    if (err.status !== 404){
+        err.status = err.status || 500
+        err.message = (err.message || "Oops! Something went wrong. Contact Support.")
+        console.log(err.status, err.message);
+        res.render('error', {error: err})
+    } else {
+        res.render('page-not-found', {error: err})
+    }
+})
+
+app.listen(3000, ()=>{
+    console.log("The app is running at port 3000.")
+})
---
+++ Project 6 Node and Express Site/routes/index.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const router = express.Router();
+const {projects} = require('../data.json');
+
+// render homepage
+router.get('/', (req, res)=>{
+    res.render('index', {projects})
+})
+
+//render about page
+router.get('/about', (req, res)=>{
+    res.render('about')
+})
+
+router.get('/project/:id', (req, res, next)=>{
+// checks if id matches any project array index before executing
+    if(projects[req.params.id]){
+        const projectId = parseFloat(req.params.id)
+        const project = projects[projectId]
+        res.render('project',{project})
+    } else {
+        const err = new Error();
+        err.status = 404;
+        err.message = `Currently, there are only ${projects.length} projects to display.`
+        next(err);
+    }
+})
+
+// Custom error; doesn't generate status code, which will automatically be allocated to 500 in handler function
+router.get('/customerror',(req, res, next)=>{
+    const err = new Error();
+    next(err)
+})
+
+
+module.exports = router;
