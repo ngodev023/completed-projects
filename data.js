fd7a8117ea77226dfacdbe80236bdc84254a1f9d
Vince Developer023 <82177794+ngodev023@users.noreply.github.com>
2021-07-10 21:34:04 -0700

    Update data.js

--- data.js
+++ data.js
@@ -6,7 +6,7 @@ const data = [
         host: "projects/project1",
         details: {
             message: "This is my first project for TreeHouse. I thought coding was going to be a cakewalk...",
-            sourceCode: "projects/project1"
+            sourceCode: "https://github.com/ngodev023/completed-projects/tree/main/projects/project1"
         }
     },
     {
@@ -16,7 +16,7 @@ const data = [
         host: "projects/project2",
         details: {
             message: "This project was supposed to teach me the basic skills in JavaScript programming. DOM manipulation, etc...",
-            sourceCode: "projects/project2"
+            sourceCode: "https://github.com/ngodev023/completed-projects/tree/main/projects/project2"
         }   
     },
     {
@@ -26,7 +26,7 @@ const data = [
         host: "projects/project3",
         details: {
             message: "Form is tricky, but essential.",
-            sourceCode: "projects/project3"
+            sourceCode: "https://github.com/ngodev023/completed-projects/tree/main/projects/project3"
         }
     },
     {
@@ -36,7 +36,7 @@ const data = [
         host: "projects/project4",
         details: {
             message: "<p>My first gander into Object Oriented Programming: Mindbending, frustrating... and then rewarding.</p> <p>There are five secret phrases; try to guess them all!</p>",
-            sourceCode: "projects/project4"
+            sourceCode: "https://github.com/ngodev023/completed-projects/tree/main/projects/project4"
         }
 
     },
@@ -47,7 +47,7 @@ const data = [
         host: "projects/project5",
         details: {
             message: "My first attempt at working with an established API. Upon load, program will reach out to an API and return 12 random profiles. Clicking each profile will open up a modal window populated by a profile's information. <p>Things may be a little tough to see in the iframe, so click on the Live Link to check it out in a full-fledged browser window.",
-            sourceCode: "projects/project5"
+            sourceCode: "https://github.com/ngodev023/completed-projects/tree/main/projects/project5"
         }
     },
     {
@@ -57,7 +57,7 @@ const data = [
         host: "index.html",
         details: {
             message: "You've actually been wandering around inside project 6 all along.",
-            sourceCode: ""
+            sourceCode: "https://github.com/ngodev023/completed-projects/tree/main/projects/project6"
         }
     },
     {
@@ -67,7 +67,7 @@ const data = [
         host: "projects/project7",
         details: {
             message: "My first attempt at working with React. <p>Challenging for sure.</p> <p>Click on the predefined search options, or type something into the filter and see what the API returns.</p> <p>Have fun!</p>",
-            sourceCode: "projects/project7"
+            sourceCode: "https://github.com/ngodev023/completed-projects/tree/main/projects/project7"
         }
     },
     {
@@ -77,7 +77,7 @@ const data = [
         host: "projects/project8",
         details: {
             message: "What I've been looking forward to throughout the length of the course: learning how to work with databases. This express application uses sql to store and manipulate data, which users can input into a mock library management user interface.",
-            sourceCode: "projects/project8"
+            sourceCode: "https://github.com/ngodev023/completed-projects/tree/main/projects/project8"
         }
     },
     {
@@ -87,7 +87,7 @@ const data = [
         host:"https://test-app023.herokuapp.com",
         details:{
             message:"<p>This is the API designed to service the next Project, a FullStack React Application. It's a little bland in terms of display, but is integral nonetheless.</p><p>If you really want to check it out, visit the live link.</p>",
-            sourceCode: "projects/project9"
+            sourceCode: "https://github.com/ngodev023/completed-projects/tree/main/projects/project9"
         }
     },
     {
@@ -97,7 +97,7 @@ const data = [
         host:"https://project10-react-app-api.netlify.app",
         details:{
             message: "<p>Finally, the graudation project comes together. This React application simulates an academic courses listing. Users may sign up for an account, and create/edit posts about a course he/she is teaching. This project is currently live and hosted by Netlify. API is hosted by Heroku.</p> <p>Note: This application currently does not support a feature for deleting user accounts. While passwords are hashed before being stored, everything else is public; it is not recommended that you use your personal or sensitive information. Feel free to play around, but remember, the app is live, so others can see what you post.</p>",
-            sourceCode: "projects/project10"
+            sourceCode: "https://github.com/ngodev023/completed-projects/tree/main/projects/project10"
         }
     }
 ];
